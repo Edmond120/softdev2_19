163629029b01bbb3fe2fcaa0716c96082e40c099
edmond120 <edmondwong888@msn.com>
2018-05-01 15:21:34 -0400

    done

--- data1.js
+++ data1.js
@@ -4,9 +4,31 @@ var total = document.getElementById("total");
 var greaterThan = document.getElementById("greaterThan");
 var percentage = document.getElementById("percentage");
 
-function getTotal(){
+function getTotal(d){
 	var add = function(num,x){return num + x["females"] + x["males"];};
-	return dataSet.reduce(add,0)
+	return d.reduce(add,0);
 }
 
-total.innerHTML = "" + getTotal()
+function getGreaterThan(d,num){
+	var filterer = function(x){return x["age"] > num;};
+	return getTotal(d.filter(filterer));
+}
+
+function getPercentage(d){
+	var t = getTotal(d);
+	var divide = function(x){return [x["age"],x["females"] / t]};
+	return d.map(divide);
+}
+
+function newLines(list){
+	var s = "age : percentage";
+	for(var i = 0; i < list.length; i++){
+		s +=   list[i][0] + " : " + (list[i][1] * 100) + "%<br>";
+	}
+	return s
+}
+
+
+total.innerHTML       = "" + getTotal(dataSet)
+greaterThan.innerHTML = "" + getGreaterThan(dataSet,40)
+percentage.innerHTML  = newLines(getPercentage(dataSet))
---
+++ makefile
@@ -0,0 +1,4 @@
+.PHONY: data1.js
+
+data1.js:
+	cat dataSet.js data.js > data1.js
